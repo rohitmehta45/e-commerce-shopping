f3ed345288162fbc61729fe8dfdd36a637e47165
rohitmehta45 <mehtarohit1012@gmail.com>
2025-12-29 23:29:35 +0545

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -13,20 +13,24 @@ import {
 import { getFirestore, collection, addDoc, query, where, getDocs, serverTimestamp } from "firebase/firestore";
 import { toast } from "react-toastify";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAt14_lQ7WskbBn38agMubT5pUHaaHQJeM",
-  authDomain: "e-commerce-94d45.firebaseapp.com",
-  projectId: "e-commerce-94d45",
-  storageBucket: "e-commerce-94d45.appspot.com",
-  messagingSenderId: "930516430438",
-  appId: "1:930516:web:30f4169a8bf0a5208c07a7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
+
+// ------------------- AUTH FUNCTIONS -------------------
 
 export const signup = async (firstName, lastName, email, password, role = "user") => {
   try {
